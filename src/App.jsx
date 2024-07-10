44809afe9ce8561cf6839d333a64751f066ee2d8
FridaSvanberg <svanberg92@gmail.com>
2024-07-10 23:37:16 +0200

    Add Results component with logic. Handle calculate fun in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
 import { useState } from 'react';
+import { calculateInvestmentResults } from './util/investment';
 
 import Header from './components/Header';
 import UserInput from './components/UserInput';
 import Results from './components/Results';
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
+    initialInvestment: 15000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
 
+  const resultData = calculateInvestmentResults(userInput);
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevValue) => {
       return {
         ...prevValue,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
+
   return (
     <>
       <Header />
       <UserInput onChangeInput={handleChange} userInput={userInput} />
-      <Results />
+      <Results userInput={userInput} annualData={resultData} />
     </>
   );
 }
